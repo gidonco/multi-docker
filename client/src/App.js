5556570f61927d424a2e055f7fedeab6a154e1f7
Hendrik van Leeuwen <gidonco@live.com>
2022-11-24 16:08:39 +0100

    client correctly added

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'; 
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Link to="/">Home</Link>
+        <Link to="/otherPage">Other Page</Link>
+      </header>
+      <div>
+        <Route exact path="/" component={Fib} />
+        <Route path="/otherPage" component={OtherPage} />
+       </div>
+    </div>
+  );
+}
+
+export default App;
